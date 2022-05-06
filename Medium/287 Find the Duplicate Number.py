c7b4dc7337b5f0e11a08b06adf7449e0ba95c6eb
Anshuman <73351827+apratyush777@users.noreply.github.com>
2022-05-06 16:10:41 +0530

    Add files via upload

---
+++ Medium/287 Find the Duplicate Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        t=h=nums[0]
+        while True:
+            t=nums[t]
+            h=nums[nums[h]]
+            if t==h:
+                break
+                
+        h=nums[0]
+        while t!=h:
+            t=nums[t]
+            h=nums[h]
+            
+        return h
