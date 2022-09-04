51c8aee263deb8817e22aa423c7e782d69c759d7
Anshuman <73351827+apratyush777@users.noreply.github.com>
2022-09-04 16:56:29 +0530

    Add files via upload

---
+++ Easy/112 Path Sum.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def helper(root,summ):
+            if not root:
+                return False
+            summ+=root.val
+            if not root.left and not root.right:
+                return summ==targetSum
+            return (helper(root.left,summ) or
+                   helper(root.right,summ))
+        return helper(root,0)
